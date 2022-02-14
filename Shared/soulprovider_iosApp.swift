71d93e74dc33dfd188fbca8bcf56e28c70152bfb
Shola Anozie <shola@soulprovidr.fm>
2022-02-14 01:38:45 -0500

    Initial Commit

---
+++ Shared/soulprovider_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  soulprovider_iosApp.swift
+//  Shared
+//
+//  Created by Shola Anozie on 2022-02-14.
+//
+
+import SwiftUI
+
+@main
+struct soulprovider_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
