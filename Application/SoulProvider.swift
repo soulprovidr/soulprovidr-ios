e054d1e96f11ec6a2d355ef19b6e4b12359261f1
Shola Anozie <shola@soulprovidr.fm>
2023-09-03 20:33:26 -0400

    Remove unnecessary variable.

--- Application/SoulProvider.swift
+++ Application/SoulProvider.swift
@@ -3,8 +3,6 @@ import SwiftUI
 
 @main
 struct SoulProvider: App {
-  private var audioSession: AVAudioSession? = nil
-  
   @StateObject private var metadataFetcher = RadioMetadataFetcherModel()
   @StateObject private var player = RadioPlayerModel()
   @StateObject private var settings = SettingsModel()
