0a37035808cbb51316c4897c9cfcdc42a3f3b5bb
Shola Anozie <shola@soulprovidr.fm>
2023-09-13 22:44:54 -0400

    Adjust active tint color of AirPlay icon.

--- Views/RadioView/DevicePickerView.swift
+++ Views/RadioView/DevicePickerView.swift
@@ -4,6 +4,7 @@ import SwiftUI
 struct DevicePickerView: UIViewRepresentable {
   func makeUIView(context: Context) -> UIView {
     let routePickerView = AVRoutePickerView()
+    routePickerView.activeTintColor = UIColor(Color("AccentColor"))
     routePickerView.tintColor = UIColor(Color("FgColor"))
     return routePickerView
   }
