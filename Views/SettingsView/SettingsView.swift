b02b7ccb505f2a28f3027c690801491481a2054e
Shola Anozie <shola@soulprovidr.fm>
2023-09-16 19:14:11 -0400

    Add icons to links in settings menu.

--- Views/SettingsView/SettingsView.swift
+++ Views/SettingsView/SettingsView.swift
@@ -14,21 +14,28 @@ struct SettingsView: View {
   }
 
   var appInformation: some View {
-    HStack {
-      Image("SPLogo")
-        .resizable()
-        .frame(width: 80, height: 80)
-        .scaledToFit()
-        .cornerRadius(16)
-      VStack(alignment: .leading, spacing: 2) {
-        Text("Soul Provider")
-          .font(.callout)
-          .fontWeight(.semibold)
-        Text("Version \(versionNumber)")
-          .font(.system(size: 14))
-        
+    VStack {
+      HStack {
+        Image("SPLogo")
+          .resizable()
+          .frame(width: 80, height: 80)
+          .scaledToFit()
+          .cornerRadius(16)
+        VStack(alignment: .leading, spacing: 2) {
+          Text("Soul Provider")
+            .font(.callout)
+            .fontWeight(.semibold)
+          Text("Version \(versionNumber)")
+            .font(.system(size: 14))
+        }
+        .padding(.leading, 10)
       }
-      .padding(.leading, 10)
+      .padding(.bottom, 15)
+      Text("\"Healing the world through the power of funk, soul, and software.\"")
+        .italic()
+        .font(.caption)
+        .frame(width: 250, alignment: .center)
+        .multilineTextAlignment(.center)
     }
     .padding([.top, .bottom], 20)
   }
@@ -39,7 +46,48 @@ struct SettingsView: View {
         appInformation
         Form {
           Section(header: Text("Links")) {
-            Link("Website", destination: URL(string: "https://soulprovidr.fm")!)
+            Link(destination: URL(string: "https://soulprovidr.fm")!) {
+              HStack {
+                Image("SPLogo")
+                  .resizable()
+                  .cornerRadius(4)
+                  .scaledToFit()
+                  .frame(width: 20, height: 20)
+                Text("Official Website")
+              }
+            }
+            Link(destination: URL(string: "https://github.com/soulprovidr")!) {
+              HStack {
+                Image("GitHubLogo")
+                  .resizable()
+                  .cornerRadius(4)
+                  .scaledToFit()
+                  .frame(width: 20, height: 20)
+                Text("GitHub")
+              }
+            }
+            Link(destination: URL(string: "https://soundcloud.com/soulprovidr")!) {
+              HStack {
+                Image("SoundCloudLogo")
+                  .resizable()
+                  .cornerRadius(4)
+                  .scaledToFit()
+                  .frame(width: 20, height: 20)
+                Text("SoundCloud")
+              }
+            }
+              Link(destination: URL(string: "https://youtube.com/@soulprovidr")!) {
+                HStack {
+                  Image("YouTubeLogo")
+                    .resizable()
+                    .cornerRadius(4)
+                    .scaledToFit()
+                    .frame(width: 20, height: 20)
+                  Text("YouTube")
+                }
+              }
+          }
+          Section(header: Text("Legal")) {
             Link("Privacy Policy", destination: URL(string: "https://soulprovidr.fm/privacy")!)
           }
         }
